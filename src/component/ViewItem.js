d6105ffa5fa52a0333064688a18778c5a666b3e6
cheatchansovoth <cheatchansovoth@gmail.com>
2023-04-18 15:11:33 +1200

    Update ViewItem.js

--- src/component/ViewItem.js
+++ src/component/ViewItem.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Data from "./files/Data.json";
+import axios from "axios";
 export const ViewItem = () => {
   const { id } = useParams();
+  const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    const fetchData = await;
+    axios
+      .get("https://nicetech.onrender.com/product/getProduct")
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
+  console.log(data);
   return (
     <div>
       <div className="w-screen min-h-screen flex justify-center items-center">
